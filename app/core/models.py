6f0368d2a28ad991af8ce239206e6c62b2b934ed
Moein <mohammadmoein.moradi@gmail.com>
2021-04-09 16:45:51 +0430

    chore: normalize email for new user

--- app/core/models.py
+++ app/core/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 class UserManager(BaseUserManager):
 	def create_user(self, email, password=None, **extra_fields):
 		"""Create and save new user"""
-		user = self.model(email=email, **extra_fields)
+		user = self.model(email=self.normalize_email(email), **extra_fields)
 		user.set_password(password)
 		user.save(using=self._db)
 		return user
