06402064f18a8322f8d330bec99d12a90d7bba05
Moein <mohammadmoein.moradi@gmail.com>
2021-04-10 16:18:26 +0430

    fix: replace tabs with spaces & fix flake8 error

--- app/core/models.py
+++ app/core/models.py
@@ -1,33 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,\
-	BaseUserManager, PermissionsMixin
+    BaseUserManager, PermissionsMixin
 
 
 class UserManager(BaseUserManager):
-	def create_user(self, email, password=None, **extra_fields):
-		"""Create and save new user"""
-		if not email:
-			raise ValueError('Email address is required')
-		user = self.model(email=self.normalize_email(email), **extra_fields)
-		user.set_password(password)
-		user.save(using=self._db)
-		return user
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save new user"""
+        if not email:
+            raise ValueError('Email address is required')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-	def create_superuser(self, email, password):
-		"""Create and save new superuser"""
-		user = self.create_user(email, password)
-		user.is_staff = True
-		user.is_superuser = True
-		user.save(using=self._db)
-		return user
+    def create_superuser(self, email, password):
+        """Create and save new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-	"""User model using email instead of username"""
-	email = models.EmailField(max_length=255, unique=True)
-	name = models.CharField(max_length=255)
-	is_active = models.BooleanField(default=True)
-	is_staff = models.BooleanField(default=False)
+    """User model using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-	objects = UserManager()
-	USERNAME_FIELD = 'email'
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,37 +3,40 @@ from django.contrib.auth import get_user_model
 
 
 class ModelTests(TestCase):
-	def test_create_user_with_email(self):
-		"""Test creating new user with email"""
-		email = 'test@gmail.com'
-		password = 'tESt1!1'
-		user = get_user_model().objects.create_user(
-			email=email,
-			password=password
-		)
+    def test_create_user_with_email(self):
+        """Test creating new user with email"""
+        email = 'test@gmail.com'
+        password = 'tESt1!1'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
 
-		self.assertEqual(user.email, email)
-		self.assertTrue(user.check_password(password))
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
 
-	def test_new_user_email_normalizedd(self):
-		"""Test email for new user is normalized"""
-		email = 'test@GMAIL.COM'
-		user = get_user_model().objects.create_user(
-			email=email,
-			password='x'
-		)
-		self.assertEqual(user.email, email.lower())
+    def test_new_user_email_normalizedd(self):
+        """Test email for new user is normalized"""
+        email = 'test@GMAIL.COM'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password='x'
+        )
+        self.assertEqual(user.email, email.lower())
 
-	def test_new_user_invalid_email(self):
-		"""New user with no email raises error"""
-		with self.assertRaises(ValueError):
-			get_user_model().objects.create_user(email=None, password='tESt1!1')
+    def test_new_user_invalid_email(self):
+        """New user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email=None,
+                password='tESt1!1'
+            )
 
-	def test_create_new_super_user(self):
-		"""Test creating new super user"""
-		user = get_user_model().objects.create_superuser(
-			email='test@gmail.com',
-			password='tESt1!1'
-		)
-		self.assertTrue(user.is_superuser)
-		self.assertTrue(user.is_staff)
+    def test_create_new_super_user(self):
+        """Test creating new super user"""
+        user = get_user_model().objects.create_superuser(
+            email='test@gmail.com',
+            password='tESt1!1'
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
