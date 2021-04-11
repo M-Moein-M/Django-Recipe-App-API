d258f7b4656ae014fcb202f54666ee473042d9b1
Moein <mohammadmoein.moradi@gmail.com>
2021-04-11 17:24:56 +0430

    chore: add setup for testing admin site

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,17 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@gmail.com',
+            password='admin123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@gmail.com',
+            password='user123',
+            name='test_user'
+        )
