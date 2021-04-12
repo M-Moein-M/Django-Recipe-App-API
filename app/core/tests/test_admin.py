cc3d6111680d674ef50fe6f0f4217ef7b097faf7
Moein <mohammadmoein.moradi@gmail.com>
2021-04-12 10:20:47 +0430

    test: add test for create new user page

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -31,3 +31,10 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test creating new user page"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
