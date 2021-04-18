3b0fbc23212899884e1629b576703d63ab950c4f
Moein <mohammadmoein.moradi@gmail.com>
2021-04-18 12:14:48 +0430

    chore: add user token url

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,15 @@
 from rest_framework import generics
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, AuthTokeSerialzier
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in system"""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create new token for user"""
+    serializer_class = AuthTokeSerialzier
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
