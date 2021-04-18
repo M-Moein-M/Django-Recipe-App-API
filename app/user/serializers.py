3f4a30b29046be517a2057afb0e85bc30c6ae2ac
Moein <mohammadmoein.moradi@gmail.com>
2021-04-18 17:38:57 +0430

    chore: add update to user serializer

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,6 +16,18 @@ class UserSerializer(serializers.ModelSerializer):
         """Creating new user and retuning created user"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update user"""
+        password = validated_data.pop('password', None)
+
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokeSerialzier(serializers.Serializer):
     """Serializer for user authentication object"""
