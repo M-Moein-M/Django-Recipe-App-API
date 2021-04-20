fb04198d611afd53b116526e0ad51a7724a351f6
Moein <mohammadmoein.moradi@gmail.com>
2021-04-20 12:45:45 +0430

    chore: add TagViewSet view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins, authentication, permissions
+from recipe.serializers import TagSerializer
+from core.models import Tag
 
-# Create your views here.
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    serializer_class = TagSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Tag.objects.all()
+
+    def get_queryset(self):
+        """Return tags for current authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
