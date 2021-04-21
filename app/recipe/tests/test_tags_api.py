a0912e4598b76e574ea61054699da19035348196
Moein <mohammadmoein.moradi@gmail.com>
2021-04-21 22:20:53 +0430

    test: fix testing for tag owner

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -60,4 +60,6 @@ class PrivateTagsApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
-        self.assertEqual(res.data[0]['user'], self.user.id)
+
+        t_user = Tag.objects.filter(id=res.data[0]['id'])[0]
+        self.assertEqual(t_user.user.id, self.user.id)
