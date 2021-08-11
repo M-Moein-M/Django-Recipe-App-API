4bccec5dd059d60632fbe3be0a99661e3d7feb6d
moein <mohammadmoein.moradi@gmail.com>
2021-08-11 10:30:20 +0430

    add test for name and path of uploaded image

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from core import models
+from unittest.mock import patch
 
 
 def sample_user(email='test@gmail.com', password='test-password'):
@@ -74,3 +75,14 @@ class ModelTests(TestCase):
             price=10.2,
         )
         self.assertEqual(str(recipe), recipe.title)
+
+    @patch('uuid.uuid4')
+    def test_recipe_file_name_uuid(self, mock_uuid):
+        """Test that image is saved in correct location"""
+        default_uuid = 'test-uuid'
+        mock_uuid.return_value = default_uuid
+
+        file_path = models.recipe_image_file_path(None, 'myimage.jpg')
+        exp_path = f'uploads/recipe/{default_uuid}.jpg'
+
+        self.assertEqual(file_path, exp_path)
