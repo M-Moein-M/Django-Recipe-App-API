8ac4e58ef83815147d1599ee885f27b1745a0eb1
moein <mohammadmoein.moradi@gmail.com>
2021-08-16 11:08:43 +0430

    add image_upload action

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets, mixins, authentication, permissions
 from recipe.serializers import TagSerializer, IngredientSerializer, \
-    RecipeSerializer, RecipeDetailSerializer
+    RecipeSerializer, RecipeDetailSerializer, RecipeImageSerializer
 from core.models import Tag, Ingredient, Recipe
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
@@ -47,9 +50,22 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Return appropriate serializer class"""
         if self.action == 'retrieve':
             return RecipeDetailSerializer
+        elif self.action == 'upload_image':
+            return RecipeImageSerializer
 
         return self.serializer_class
 
     def perform_create(self, serializer):
         """Create new recipe"""
         serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe,
+                                         data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
